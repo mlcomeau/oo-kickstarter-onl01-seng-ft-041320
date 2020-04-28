49185561b1d3592609ba67ca229f37ba906fb78e
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 16:34:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project
-  attr_reader :title, :backers 
+  attr_reader :title, :backers
   def initialize (title)
-    @title = title 
+    @title = title
     @backers = []
-  end 
+  end
 
-  
+
 end
